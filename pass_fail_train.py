1b0cf97392ed6c13201edda2e6d4c0bc51118c7c
iamdoky <kdh6631@gmail.com>
2019-03-29 14:53:32 +0900

    pass

--- pass_fail_train.py
+++ pass_fail_train.py
@@ -1,6 +1,45 @@
-from sklearn import svm
-
+from sklearn import svm, metrics
 data = [
-    [],
-
+    [20,100,'합격'],
+    [18,3,'불합격'],
+    [15,10,'불합격'],
+    [18,80,'합격'],
+    [18,0,'불합격'],
+    [20,90,'합격'],
+    [20,100,'합격'],
+    [17,50,'불합격'],
+    [19,90,'합격'],
+    [14,50,'불합격']
 ]
+data2 = data[:7]
+data3 = data[7:]
+
+d2 = []
+l2 = []
+d3 = []
+l3 = []
+
+for i in range(len(data2)):
+    row = data2[i]
+    days = row[0]
+    hours = row[1]
+    isPass = row[2]
+    d2.append([days,hours])
+    l2.append(isPass)
+
+for i in range(len(data3)):
+    row = data3[i]
+    days = row[0]
+    hours = row[1]
+    isPass = row[2]
+    d3.append([days,hours])
+    l3.append(isPass)
+
+clf = svm.SVC()
+clf.fit(d2,l2)
+
+r = clf.predict(d3)
+
+acc= metrics.accuracy_score(l3,r)
+print(acc)
+
